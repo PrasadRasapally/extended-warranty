691674e1e25b5c1aec670acda32b080deefa5813
unknown <rprasad@prasadr.pkrmsezdc.com>
2016-06-24 18:50:20 +0530

    updating with discount code changes.

--- src/controllers/extendedWarrantyConfirmationCtrl.js
+++ src/controllers/extendedWarrantyConfirmationCtrl.js
@@ -30,20 +30,36 @@ export default class ExtendedWarrantyReviewCtrl {
         this.isDiscountApplied = localStorage.getItem("isDiscountApplied");
         
         this.calculateTotalPrice();
-    }
+        
+        this.submittedDate = this.convertUTCDateToLocalDate(new Date(this.submittedDate)).toLocaleString();
+    }    
     calculateTotalPrice(){
         var self = this;
         self.totalPrice = 0;
-        angular.forEach(this.selectedAssets.simpleLineItems, function(value, key) {
+        angular.forEach(self.selectedAssets.simpleLineItems, function(value, key) {
             self.totalPrice += value.selectedPrice;
         });
-        angular.forEach(this.selectedAssets.compositeLineItems, function(value, key) {
+        angular.forEach(self.selectedAssets.compositeLineItems, function(value, key) {
             self.totalPrice += value.selectedPrice;
         });
         
         this.afterDiscountPrice = this.totalPrice - ( this.discountPrice || 0 );
         
-        this._$timeout(function(){ localStorage.clear(); }, 2000);        
+        if(this.discountPrice % 1 !== 0){
+            this.totalPrice = this.totalPrice.toFixed(2);
+            this.afterDiscountPrice = this.afterDiscountPrice.toFixed(2);
+        }
+        
+        this._$timeout(function(){ localStorage.clear(); }, 3000);
+    };
+    
+    convertUTCDateToLocalDate( date ) {
+        var newDate = new Date(date.getTime() + date.getTimezoneOffset()*60*1000);
+        var offset = date.getTimezoneOffset() / 60;
+        var minutes = date.getMinutes() / 60;
+        var hours = date.getHours();
+        newDate.setHours(hours - offset + minutes);
+        return newDate;
     };
 }
 
